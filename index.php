6e59ed25b90f41b6f56b1114540f9e2500bf0747
sayarozi <achmad_23510017@stimata.ac.id>
2023-11-16 17:58:40 +0700

    perintah tugas 1

---
+++ index.php
@@ -0,0 +1,12 @@
+<html>
+    <head>
+        <title>
+            Belajar PHP
+        </title>
+    </head>
+    <body>
+        <h1>
+            Selamat Datang <?php echo "Rozi";?>
+        </h1>
+    </body>
+</html>
