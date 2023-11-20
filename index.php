283eafd13ed29a567af4c4e2b49ee7178c76f3af
sayarozi <achmad_23510017@stimata.ac.id>
2023-11-20 20:18:31 +0700

    perintah tugas 6

--- index.php
+++ index.php
@@ -1,11 +1,8 @@
 <?php
-//Operator Aritmatika
-$x = 10;
-$y = 20;
-    echo $x + $y ;  #operator penjumlahan
-    echo $x - $y ;  #operator pengurangan
-    echo $x * $y ;  #operator perkalian
-    echo $x / $y ;  #operator pembagian
-    echo $x % $y ;  #operator modulus
+
+$nama_depan = "Achmad Nur";
+$nama_belakang = "Rozikin";
+
+echo $nama_depan . "" . $nama_belakang;
 
 ?>
