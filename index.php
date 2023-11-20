fb185c3769ff0b929d545f681ed85c091ddae5cc
sayarozi <achmad_23510017@stimata.ac.id>
2023-11-20 20:23:13 +0700

    perintah tugas 8

--- index.php
+++ index.php
@@ -1,6 +1,29 @@
 <?php
-//operator aritmatika
-$x = 33;
-$y = 90;
-echo $x * $y ; #operator perkalian
+//operator perbandingan
+var_dump (1 < 5);
+echo "</br>";       #output baris ke-1
+var_dump (1 > 5);
+echo "</br>";       #output baris ke-2
+var_dump (1 <= 5);
+echo "</br>";       #output baris ke-3
+var_dump (1 >= 5);
+echo "</br>";       #output baris ke-4
+var_dump (1 == 5);
+echo "</br>";       #output baris ke-5
+var_dump (1 != 5);
+echo "</br>";       #output baris ke-6
+
+//operator identitas
+var_dump (1 === 5); #output baris ke-7
+echo "</br>"; 
+var_dump (1 !== 5); #output baris ke-8
+
+//operator logika
+echo "</br>"; 
+$x = 10;
+echo var_dump ($x < 20 && $x % 2 == 0) . "</br>";   #output baris ke-9
+$x = 10;
+
+echo var_dump ($x < 20 || $x % 2 == 0) . "</br>";   #output baris ke-10
+
 ?>
