35bd87267df88a3e5442cdaa6ad6b8a90b8f2086
sayarozi <achmad_23510017@stimata.ac.id>
2023-12-05 22:32:10 +0700

    task 1 date

---
+++ date.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>belajar php tentang date</title>
+</head>
+
+<body>
+    <h1>
+        <?php
+        echo date("M");
+        ?> </h1>
+</body>
+
+</html>
