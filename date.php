07c7ec0d2714e437202a703653b1543d159862f7
sayarozi <achmad_23510017@stimata.ac.id>
2024-01-02 19:54:11 +0700

    task 6

--- date.php
+++ date.php
@@ -1,10 +1,32 @@
 <?php
 
-$harilahir = mktime(0,0,0,6,9,1996);
+function 
+hari_ulang_tahun($tahun,
+$bulan, $tanggal) {
+    //membuat timestamp dari tanggal ulang tahun menggunakan mktime
+$timestamp_ulang_tahun = mktime(0,0,0, $bulan, $tanggal, $tahun);
 
-$hari = date ("1",$minggu);
-$tanggal = date ("d",09);
-$bulan = date ("m",$06);
-$tahun = date ("y",$1996);
+    //mengambil nama hari dari timestamp
+$hari_ulang_tahun = date("l", $timestamp_ulang_tahun);  
 
-echo "Tanggal lahir":"$minggu","9","06","1996"
+    return 
+$hari_ulang_tahun;
+}
+
+//menentukan tanggal ulang tahun
+$tanggal_ulang_tahun = 9;
+$bulan_ulang_tahun = 6;
+$tahun_ulang_tahun = 1996;
+
+//menentukan tahun untuk mencari hari ulang tahun 
+$tahun_target = 2030;
+
+//memanggil fungsi untuk menemukan hari ulang tahun 
+$hasil_hari_ulang_tahun = 
+hari_ulang_tahun($tahun_target, $bulan_ulang_tahun, $tanggal_ulang_tahun);
+
+echo "Hari ulang tahun pada tanggal 
+$tanggal_ulang_tahun bulan $bulan_ulang_tahun tahun $tahun_target adalah 
+$hasil_hari_ulang_tahun.";
+
+?>
